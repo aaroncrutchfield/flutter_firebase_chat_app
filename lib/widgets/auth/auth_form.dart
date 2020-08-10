fd436e8187c9a3f4d38ec43de4860650eaba3b87
acrutchfield <aaroncrutchfield09@gmail.com>
2020-08-10 19:42:54 -0400

    Create a user auth form

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -6,42 +6,95 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+
+  String _userEmail = '';
+  String _username = '';
+  String _password = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState.save();
+      print(_userEmail);
+      print(_username);
+      print(_password);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-	    child: Card(
-		    margin: EdgeInsets.all(20),
-		    child: SingleChildScrollView(
-			    child: Padding(
-				    padding: EdgeInsets.all(16),
-				    child: Form(
-							child: Column(
-								mainAxisSize: MainAxisSize.min,
-								children: <Widget>[
-									TextFormField(
-										keyboardType: TextInputType.emailAddress,
-										decoration: InputDecoration(labelText: 'Email address'),
-									),
-									TextFormField(
-										decoration: InputDecoration(labelText: 'Username'),
-									),
-									TextFormField(
-										decoration: InputDecoration(labelText: 'Password'),
-										obscureText: true,
-									),
-									SizedBox(height: 12),
-									RaisedButton(child: Text('Login'), onPressed: () {
-
-									},),
-									FlatButton(textColor: Theme.of(context).primaryColor,child: Text('Create new account',), onPressed: (){
-
-									},)
-								],
-							),
-				    ),
-			    ),
-		    ),
-	    ),
+      child: Card(
+        margin: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  TextFormField(
+                    key: ValueKey('email'),
+                    validator: (emailInput) {
+                      if (emailInput.isEmpty || !emailInput.contains('@')) {
+                        return 'Please enter a valid email address.';
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(labelText: 'Email address'),
+                    onSaved: (emailInput) => _userEmail = emailInput,
+                  ),
+                  if (!_isLogin)
+                  TextFormField(
+                    key: ValueKey('username'),
+                    validator: (usernameInput) {
+                      if (usernameInput.isEmpty || usernameInput.length < 4) {
+                        return 'Username must be at least 4 characters long.';
+                      }
+                      return null;
+                    },
+                    onSaved: (usernameInput) => _username = usernameInput,
+                    decoration: InputDecoration(labelText: 'Username'),
+                  ),
+                  TextFormField(
+                    key: ValueKey('password'),
+                    validator: (passwordInput) {
+                      if (passwordInput.isEmpty || passwordInput.length < 7) {
+                        return 'Password must be at least 7 characters long.';
+                      }
+                      return null;
+                    },
+	                  onSaved: (passwordInput) => _password = passwordInput,
+	                  decoration: InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                  ),
+                  SizedBox(height: 12),
+                  RaisedButton(
+                    key: ValueKey('login'),
+                    child: Text(_isLogin ? 'Login' : 'Signup'),
+                    onPressed: _trySubmit,
+                  ),
+                  FlatButton(
+                    textColor: Theme.of(context).primaryColor,
+                    child: Text(_isLogin ?
+                      'Create new account' : 'I already have an account',
+                    ),
+                    onPressed: () {
+                      setState(() =>_isLogin = !_isLogin);
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
