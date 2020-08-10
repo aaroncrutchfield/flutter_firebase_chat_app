c636788b6459004ce36147dc45646950131f484c
acrutchfield <aaroncrutchfield09@gmail.com>
2020-08-10 18:57:01 -0400

    Test adding data

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -26,7 +26,11 @@ class ChatScreen extends StatelessWidget {
 
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          Firestore.instance.collection('chats/Ye2HS2iYXdjSmuq6vWoF/messages').add({
+            'text': 'This was added from the FAB'
+          });
+        },
       ),
     );
   }
