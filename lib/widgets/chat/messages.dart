12e9fb9780114343a4cca136fca969f61ec8a438
acrutchfield <aaroncrutchfield09@gmail.com>
2020-08-12 21:16:52 -0400

    Create a basic chat bubble

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,22 +1,30 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_chat_app/widgets/chat/message_bubble.dart';
 
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-	    stream: Firestore.instance.collection('chat').orderBy('createdAt', descending: true).snapshots(),
-	    builder: (ctx, chatSnapshot) {
-	    	if (chatSnapshot.connectionState == ConnectionState.waiting) {
-	    		return Center(child: CircularProgressIndicator(),);
-		    }
-	    	final chatDocs = chatSnapshot.data.documents;
-	    	return ListView.builder(
-			    reverse: true,
-			    itemCount: chatDocs.length,
-			    itemBuilder: (ctx, index) => Text(chatDocs[index]['text']),);
-	    },
+      stream: Firestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: true)
+          .snapshots(),
+      builder: (ctx, chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final chatDocs = chatSnapshot.data.documents;
+        return ListView.builder(
+          reverse: true,
+          itemCount: chatDocs.length,
+          itemBuilder: (ctx, index) =>
+              MessageBubble(message: chatDocs[index]['text']),
+        );
+      },
     );
   }
 }
