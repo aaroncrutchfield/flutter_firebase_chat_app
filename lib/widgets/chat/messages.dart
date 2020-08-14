8274e85d64ec5eca1f2f05ed5ac8e40947addb6f
acrutchfield <aaroncrutchfield09@gmail.com>
2020-08-13 22:55:01 -0400

    Improve the chat bubble

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_chat_app/widgets/chat/message_bubble.dart';
@@ -6,24 +7,31 @@ import 'package:flutter_firebase_chat_app/widgets/chat/message_bubble.dart';
 class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Firestore.instance
-          .collection('chat')
-          .orderBy('createdAt', descending: true)
-          .snapshots(),
-      builder: (ctx, chatSnapshot) {
-        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+    return FutureBuilder(
+      future: FirebaseAuth.instance.currentUser(),
+      builder: (ctx, futureSnapshot) {
+        if (futureSnapshot.connectionState == ConnectionState.waiting) {
           return Center(
             child: CircularProgressIndicator(),
           );
         }
-        final chatDocs = chatSnapshot.data.documents;
-        return ListView.builder(
-          reverse: true,
-          itemCount: chatDocs.length,
-          itemBuilder: (ctx, index) =>
-              MessageBubble(message: chatDocs[index]['text']),
-        );
+        return StreamBuilder(
+            stream: Firestore.instance
+                .collection('chat')
+                .orderBy('createdAt', descending: true)
+                .snapshots(),
+            builder: (ctx, chatSnapshot) {
+              final chatDocs = chatSnapshot.data.documents;
+              return ListView.builder(
+                reverse: true,
+                itemCount: chatDocs.length,
+                itemBuilder: (ctx, index) => MessageBubble(
+                  message: chatDocs[index]['text'],
+                  isMe: chatDocs[index]['userId'] == futureSnapshot.data.uid,
+                  key: ValueKey(chatDocs[index].documentID),
+                ),
+              );
+            });
       },
     );
   }
