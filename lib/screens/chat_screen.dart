5b1d609969b3cb5da7bcdfdfcaa1835f89b465db
acrutchfield <aaroncrutchfield09@gmail.com>
2020-08-15 09:56:45 -0400

    Configure ios permissions for push notifications

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_chat_app/widgets/chat/messages.dart';
 import 'package:flutter_firebase_chat_app/widgets/chat/new_message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+    final firebaseMessaging = FirebaseMessaging();
+    firebaseMessaging.requestNotificationPermissions();
+    firebaseMessaging.configure();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
